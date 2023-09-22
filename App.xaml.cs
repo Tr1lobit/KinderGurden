ab2a96b4a954871aacfe5ec9c2c82f8a79935c28
Антон Афоничев <pushatrol@gmail.com>
2023-09-22 13:46:49 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using AfonichevKinderGarden.Model;
+
+namespace AfonichevKinderGarden
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static KinderGardenEntities _context;
+
+        public static KinderGardenEntities GetContext()
+        {
+            if(_context==null)
+            {
+                _context = new KinderGardenEntities();
+            }
+            return _context;
+        }
+	} 
+}
---
+++ PageMain/PageBody.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AfonichevKinderGarden.PageMain
+{
+    /// <summary>
+    /// Interaction logic for PageBody.xaml
+    /// </summary>
+    public partial class PageBody : Page
+    {
+        public PageBody()
+        {
+            InitializeComponent();
+        }
+    }
+}
