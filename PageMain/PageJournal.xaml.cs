2574b0962d320dd55bfb2573568a66c5513aed93
Антон Афоничев <pushatrol@gmail.com>
2023-10-23 13:54:51 +0300

    Добавлена связь комбобоксов в журнале

--- PageMain/PageJournal.xaml.cs
+++ PageMain/PageJournal.xaml.cs
@@ -24,10 +24,25 @@ namespace AfonichevKinderGarden.PageMain
         public PageJournal()
         {
             InitializeComponent();
-            ActivityCmb.ItemsSource = App.GetContext().Activity.ToList();
-            ActivityTypeCmb.ItemsSource = App.GetContext().Direction.ToList();
-            GroupCmb.ItemsSource = App.GetContext().GroupDS.ToList();
+
+            GroupTypeCmb.SelectedValuePath = "Id";
+            GroupTypeCmb.DisplayMemberPath = "Name";
             GroupTypeCmb.ItemsSource = App.GetContext().VidGroup.ToList();
+
+            GroupCmb.SelectedValuePath = "Id";
+            GroupCmb.DisplayMemberPath = "Name";
+            GroupCmb.ItemsSource = App.GetContext().GroupDS.ToList();
+
+            ActivityTypeCmb.SelectedValuePath = "Id";
+            ActivityTypeCmb.DisplayMemberPath = "Name";
+            ActivityTypeCmb.ItemsSource = App.GetContext().Direction.ToList();
+
+            ActivityCmb.SelectedValuePath = "Id";
+            ActivityCmb.DisplayMemberPath = "Name";
+            ActivityCmb.ItemsSource = App.GetContext().Activity.ToList();
+
+            GradeCmb.SelectedValuePath = "Id";
+            GradeCmb.DisplayMemberPath = "Name";
             GradeCmb.ItemsSource = App.GetContext().Mark.ToList();
         }
 
@@ -37,19 +52,19 @@ namespace AfonichevKinderGarden.PageMain
 
             if (string.IsNullOrWhiteSpace(DateDP.Text))
                 mes += "Выберите дату\n";
-            
+
             if (string.IsNullOrWhiteSpace(GroupTypeCmb.Text))
                 mes += "Выберите вид группы\n";
-            
+
             if (string.IsNullOrWhiteSpace(GroupCmb.Text))
                 mes += "Выберите группу\n";
-            
+
             if (string.IsNullOrWhiteSpace(ActivityTypeCmb.Text))
                 mes += "Выберите вид мероприятия\n";
-            
+
             if (string.IsNullOrWhiteSpace(ActivityCmb.Text))
                 mes += "Выберите мероприятие\n";
-            
+
             if (string.IsNullOrWhiteSpace(GradeCmb.Text))
                 mes += "Выберите оценку\n";
 
@@ -60,6 +75,18 @@ namespace AfonichevKinderGarden.PageMain
         private void BackBtn_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new PageBody());
+        }
+
+        private void GroupTypeCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedGT = Convert.ToInt32(GroupTypeCmb.SelectedValue);
+            GroupCmb.ItemsSource = App.GetContext().GroupDS.Where(x => x.IdVidGroup == selectedGT).ToList();
+        }
+
+        private void ActivityTypeCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedAT = Convert.ToInt32(ActivityTypeCmb.SelectedValue);
+            ActivityCmb.ItemsSource = App.GetContext().Activity.Where(x => x.IdDirection == selectedAT).ToList();
         }
     }
 }
